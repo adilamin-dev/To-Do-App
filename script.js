c108ab25ac575dc1cbf647ea1830c17cb2a939b4
adilamin-dev <adilamin537@gmail.com>
2022-09-09 12:04:45 -0400

    To do app design completed.

---
+++ script.js
@@ -0,0 +1,13 @@
+var myInput = document.getElementById("task-input");
+
+// var creatTask = function("div", "task-box", "id", "task"){
+    
+    
+// } 
+
+if(myInput.value != null){
+    document.createElement("div");
+    var nTaskElement = document.createElement("div");
+    nTaskElement.className("task-box");
+    nTaskElement.setAttribute("id", "task");
+};
